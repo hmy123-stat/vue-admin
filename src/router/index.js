b77fcd75a867299ef808f814ebf70290e18bf03f
hmy123-stat <1019523319@qq.com>
2021-02-24 15:54:58 +0800

    创建项目、配置全局样式、路由、elementui

---
+++ src/router/index.js
@@ -0,0 +1,37 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+// import Home from "../views/Home.vue";
+// import Login  from "../views/Login/index.vue";
+Vue.use(VueRouter);
+
+const routes = [
+  {
+    path: "/",
+    redirect: "login"
+
+    // name: "Home",
+    // component: Home
+    // //对应的是Home.vue中的"Welcome to Your Vue.js App",这个目录是根目录路径
+  },
+  {
+    path: "/login",
+    name: "Login",
+    component: () => import("../views/Login/index.vue")
+  },
+  // {
+  //   path: "/about",
+  //   name: "About",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/About.vue")
+  //     //对应的是About.vue中的
+  // }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
---
+++ src/views/Login/index.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="login"> 
+        <div  class="login_1">
+            <el-button>登录</el-button>
+            <el-button>注册</el-button>
+        </div>
+        <br>
+        <div>
+            邮箱<br>
+            <input type="text">
+        </div>
+        
+    </div>
+</template>
+<script>
+export default{
+    name:'login'
+}
+</script>
+<style lang="scss" scoped>
+#login{
+    height: 100vh;
+    background-color: #344a5f;
+
+}
+.el-button{
+    background-color: #142d45;
+    color:white;
+    outline: none;
+    display: inline-block;
+    margin:0 auto;
+}
+
+</style>
